d861ef4f84cd28a8fcb910a0d03a6ae2f81c4930
Ruby Hsiao <hrhsiao@gmail.com>
2024-06-28 18:56:46 +0000

    practice 6/22 PM class

--- lesson10/index1.py
+++ lesson10/index1.py
@@ -1,6 +1,6 @@
 import requests
 
-youbike_url = ""
+youbike_url = "https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?size=2000"
 
 try:
     response = requests.get(youbike_url)
